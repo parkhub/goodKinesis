2a51f41cb332180514082669f2dfde9e4894d9ca
Fliko <baxter.flw@gmail.com>
2018-02-19 16:56:09 -0600

    change dir

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const kinesis = require('./index.js').GoodKinesis;
+const kinesis = require('./lib').GoodKinesis;
 
 let k = new kinesis({threshold: 1,
                   tags: ['obe', 'server.settings.app.version', 'fox-services-testing'],
