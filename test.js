737ecb8d3d321cffe9c3f8be9c928f3276562668
Fliko <baxter.flw@gmail.com>
2018-02-21 08:33:56 -0600

    change date stream, add test

--- test.js
+++ test.js
@@ -1,8 +1,19 @@
+process.env.AWS_S3_REGION = 'us-east-1';
+process.env.AWS_KINESIS_KEY = '123';
+process.env.AWS_KINESIS_SECRET = '321';
 const kinesis = require('./lib').GoodKinesis;
 
-let k = new kinesis({threshold: 1,
+let k = new kinesis({threshold: 5,
                   tags: ['obe', 'server.settings.app.version', 'fox-services-testing'],
                   streamName: 'llVool',
                   partitionKey: '123'});
 
-console.log(k);
+k._write({
+  event: 'log',
+  timestamp: 1519222686435,
+  tags: ['info', 'db'],
+  data: 'Configuring database',
+  pid: 40
+},
+'utf8',
+  () => console.log(k._buffer));
